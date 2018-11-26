0540a314cd48c98378fccde75c809ab2aba620a8
Mohammad Jakaria Istauk <jakariamd35@gmail.com>
2018-11-27 00:09:13 +0600

    .py file modified

--- snakeGame.py
+++ snakeGame.py
@@ -20,11 +20,67 @@ class cube(object):
 
 
 class snake(object):
+	body = []
+	turns = {}
 	def __init__(self, color, pos):
-		pass
+		self.color = color
+		self.head = cube(pos)
+		self.body = append(self.head)
+		self.dirnx = 0
+		self.dirny = 1
 
 	def move(self):
-		pass
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+			keys = pygame.key.get_pressed()
+
+			for key in keys:
+				if keys[pygame.K_LEFT]:
+					self.dirnx = -1
+					self.dirny = 0
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+				elif keys[pygame.K_RIGHT]:
+					self.dirnx = 1
+					self.dirny = 0
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+				elif keys[pygame.K_UP]:
+					self.dirnx = 0
+					self.dirny = -1
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+				elif keys[pygame.K_DOWN]:
+					self.dirnx = 0
+					self.dirny = 1
+					self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+		for i, c in enumerate(self.body):
+			p = c.pos[:]
+			if p in self.turns:
+				turns = self.turns[p]
+				c.move(turns[0], turns[1])
+				if i == len(self.body)-1:
+					self.turns.pop(p)
+
+			else:
+				if c.dirnx == -1 and c.pos[0] <= 0:
+					c.pos = (c.rows-1, c.pos[1])
+
+				elif c.dirnx == 1 and c.pos[0] >= c.rows-1:
+					c.pos = (0, c.pos[1])
+
+				elif c.dirny == 1 and c.pos[1] >= c.rows-1:
+					c.pos = (c.pos[0], 0)
+
+				elif c.dirny == -1 and c.pos[1] <= 0:
+					c.pos = (c.pos[0],c.rows-1)
+
+				else:
+					c.move(c.dirnx, c.dirny)
+
 
 	def reset(self, pos):
 		pass
@@ -36,4 +92,48 @@ class snake(object):
 		pass
 
 def drawGird(w, rows, surface):
+	sizebtwn = w // rows
+
+	x = 0
+	y = 0
+
+	for l in range(rows):
+		x = x + sizebtwn
+		y = y + sizebtwn 
+
+		pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+		pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
+
+def redrowWindow(surface):
+	global rows, width
+	surface.fill((0,0,0))
+	drawGird(width, rows, surface)
+	pygame.display.update()
+
+def randomSnake(rows, items):
 	pass
+
+def message_box():
+	pass
+
+def main():
+	global rows, width
+	width = 500
+	rows = 20
+	win = pygame.display.set_mode((width, width))
+	s = snake((255,0,0), (10,10))
+	flag = True
+
+	clock = pygame.time.Clock()
+
+	while flag:
+		pygame.time.delay(50)
+		clock.tick(10)
+		redrowWindow(win)
+	pass
+
+
+
+
+
+main()
